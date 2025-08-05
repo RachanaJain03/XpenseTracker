4fc7430d7f9b83013762f277453dc9d564d21c42
RachanaJain03 <rachu143nagar@gmail.com>
2025-08-05 13:02:33 +0530

    Headeradded

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,9 +1,24 @@
 import React from "react";
 
-const Header = ()=> {
-    <h1 style={{color: "#fff", textAlign: "center", marginBottom: "20px"}}>
-        Expense Tracker
-    </h1>
-}
+const Header = () => {
+  return (
+    <header style={styles.header}>
+      <h1 style={styles.title}>Expense Tracker</h1>
+    </header>
+  );
+};
+
+const styles = {
+  header: {
+    backgroundColor: "#282c34",
+    padding: "20px",
+    textAlign: "center",
+  },
+  title: {
+    color: "white",
+    fontSize: "2rem",
+    margin: 0,
+  },
+};
 
 export default Header;
